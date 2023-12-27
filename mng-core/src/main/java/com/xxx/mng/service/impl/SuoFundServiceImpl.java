1f42470cc51f4f6f9571e05d950b688621950715
yang-m <yangyalong90@163.com>
2023-12-27 17:13:55 +0800

    各类型查询

--- mng-core/src/main/java/com/xxx/mng/service/impl/SuoFundServiceImpl.java
+++ mng-core/src/main/java/com/xxx/mng/service/impl/SuoFundServiceImpl.java
@@ -3,9 +3,7 @@ package com.xxx.mng.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.xxx.common.utils.UUIDUtil;
-import com.xxx.mng.entities.SuoPzFundCheck;
-import com.xxx.mng.entities.SuoPzFundCheckRecordEntity;
-import com.xxx.mng.entities.SuoPzRechargeEntity;
+import com.xxx.mng.entities.*;
 import com.xxx.mng.mapper.SuoFundMapper;
 import com.xxx.mng.service.SuoFundService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +25,12 @@ public class SuoFundServiceImpl implements SuoFundService {
     public static final Map<String, TypeFund> TYPE_FUND_MAP = new HashMap<>();
     static {
         TYPE_FUND_MAP.put("充值", new TypeFund("pz充值", SuoPzRechargeEntity.class));
+        TYPE_FUND_MAP.put("打赏", new TypeFund("pz打赏", SuoPzRewardEntity.class));
+        TYPE_FUND_MAP.put("换课", new TypeFund("pz换课", SuoPzChangeSourceEntity.class));
+        TYPE_FUND_MAP.put("会员", new TypeFund("pz会员", SuoPzMemberEntity.class));
+        TYPE_FUND_MAP.put("视频", new TypeFund("pz视频", SuoPzVideoEntity.class));
+        TYPE_FUND_MAP.put("投资大学", new TypeFund("pz投资大学", SuoPzUniversityEntity.class));
+        TYPE_FUND_MAP.put("专栏", new TypeFund("pz专栏", SuoPzSpecialEntity.class));
     }
 
     @Autowired
