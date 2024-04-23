652b8c585610043e5be574bedb2e6904b5214f17
yang-m <yangyalong90@163.com>
2024-04-23 18:09:35 +0800

    修改项目结构

---
+++ mng/mng-core/src/main/java/com/xxx/mng/kafka/KafkaProducer.java
@@ -0,0 +1,56 @@
+package com.xxx.mng.kafka;
+
+import org.apache.commons.math3.util.MathUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.Random;
+
+/**
+ * @author a1234
+ * @description
+ * @create 2024-01-14 18:37
+ */
+//@Component
+public class KafkaProducer {
+
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    @Autowired
+    ThreadPoolTaskExecutor taskExecutor;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void startSend() {
+        taskExecutor.execute(() -> {
+            Random random = new Random();
+            while (true) {
+                try {
+                    // 生成一个指定范围内的随机整数（包括边界值）
+                    int minValue = 1;
+                    int maxValue = 200;
+                    int randomInt = random.nextInt(maxValue - minValue + 1) + minValue;
+                    System.out.println("随机整数：" + randomInt);
+                    send(randomInt+"");
+                    Thread.sleep(randomInt);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public void send(String msg) {
+        // 发送消息
+        kafkaTemplate.send("test_flink_topic", msg);
+    }
+
+}
